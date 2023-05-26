181f90b90226f4d98f14a76ea078c9166be9fb3f
Zach Petch <zachpetch@gmail.com>
2023-05-26 00:52:52 -0700

    Initial Commit

---
+++ Genremix/GenremixApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenremixApp.swift
+//  Genremix
+//
+//  Created by McHobbes on 2023-05-26.
+//
+
+import SwiftUI
+
+@main
+struct GenremixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
